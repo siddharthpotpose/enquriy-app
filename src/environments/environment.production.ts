722dff78bc4d0a66abf162ced6b66ab458ae6dad
SiddharthPotpose <siddharthpotpose9@gmail.com>
2026-02-09 20:03:43 +0530

    new changes

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://api.freeprojectapi.com/api/Enquiry/',
   apiLoginUrl: 'https://api.freeprojectapi.com/api/UserApp/'
 };
